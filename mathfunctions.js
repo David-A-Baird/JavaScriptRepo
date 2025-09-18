433a405df73b0c65e4458156829e2a9e41f97afa
David-A-Baird <davandybair@gamil.com>
2025-09-18 16:40:32 -0600

    Math stuff done

---
+++ mathfunctions.js
@@ -0,0 +1,16 @@
+function circleArea(radius) {
+    return radius * radius * Math.PI
+}
+
+function passwordRandom(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return password;
+}
+
+function totalCost(dollars, taxPercent) {
+    return dollars + dollars * taxPercent
+}
